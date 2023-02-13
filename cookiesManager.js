a4601440a13048c79fde1e5b9dbbc5f44c89f1bc
Spookesites <94997356+Spookesites@users.noreply.github.com>
2023-02-13 17:29:33 -0500

    Add files via upload

---
+++ cookiesManager.js
@@ -0,0 +1,39 @@
+// Function to create a cookie
+function createCookie(name, value, days) {
+    let expires = "";
+    if (days) {
+      let date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+  }
+  
+  
+  // Function to create a cookie With Infinite time/days    
+  function createCookieInfinite(name, value, days) {
+    let expires = "";
+    if (days || days === 0) {
+      let date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+  }
+  
+  // Function to read a cookie
+  function readCookie(name) {
+    let nameEQ = name + "=";
+    let ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+  }
+  
+  // Function to delete a cookie
+  function deleteCookie(name) {
+    createCookie(name, "", -1);
+  }
